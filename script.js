65384e8ac645818f9fd0a6ffd993c884db9df655
MauricioCamposOrozco <al45090@un.edu.mx>
2025-11-10 20:41:02 -0600

    Update

--- script.js
+++ script.js
@@ -4,42 +4,28 @@ var formPaquetes = document.getElementById("paquetes");
 function calcularIndividual(e) {
     var total = 0;
 
-    if (formIndividual.vuelo.value == "sin-avion")
-    {
+    if (formIndividual.vuelo.value == "sin-avion") {
         total += 999;
-    }
-    else if (formIndividual.vuelo.value == "ida")
-    {
+    } else if (formIndividual.vuelo.value == "ida") {
         total += 2499;
-    }
-    else if (formIndividual.vuelo.value == "ida-vuelta")
-    {
+    } else if (formIndividual.vuelo.value == "ida-vuelta") {
         total += 5999;
     }
 
-    if (formIndividual.hospedaje.value == "sin-hotel")
-    {
+    if (formIndividual.hospedaje.value == "sin-hotel") {
         total += 499;
-    }
-    else if (formIndividual.hospedaje.value == "sencilla")
-    {
+    } else if (formIndividual.hospedaje.value == "sencilla") {
         total += 799;
-    }
-    else if (formIndividual.hospedaje.value == "doble")
-    {
+    } else if (formIndividual.hospedaje.value == "doble") {
         total += 999;
     }
 
     total += formIndividual.adultos.value * 865;
-
     total += parseInt(formIndividual.menores.value) * 437;
-    
-    if (!formIndividual.diasIndividual.value) 
-    {
-        alert("Escriba el Número de Días en la sección de Individual");
-    }
 
-    total += parseInt(formIndividual.diasIndividual.value) * 769;
+    if (formIndividual.diasIndividual.value) {
+        total += parseInt(formIndividual.diasIndividual.value) * 769;
+    }
 
     formIndividual.totalIndividual.value = total.toLocaleString('es-MX');
 }
@@ -47,64 +33,62 @@ function calcularIndividual(e) {
 function calcularPaquetes(e) {
     var total = 0;
 
-    if (formPaquetes.destino.value == "suramerica")
-    {
+    if (formPaquetes.destino.value == "suramerica") {
         total += 7999;
-    }
-    else if (formPaquetes.destino.value == "europa")
-    {
+    } else if (formPaquetes.destino.value == "europa") {
         total += 21999;
-    }
-    else if (formPaquetes.destino.value == "africa")
-    {
+    } else if (formPaquetes.destino.value == "africa") {
         total += 17999;
-    }
-    else if (formPaquetes.destino.value == "asia")
-    {
+    } else if (formPaquetes.destino.value == "asia") {
         total += 25999;
-    }
-    else if (formPaquetes.destino.value == "norteamerica")
-    {
+    } else if (formPaquetes.destino.value == "norteamerica") {
         total += 12999;
     }
 
-    if (!formPaquetes.diasPaquete.value) 
-    {
-        alert("Escriba el Número de Días en la seccón de Paquetes");
-        return;
+    if (formPaquetes.diasPaquete.value) {
+        total += parseInt(formPaquetes.diasPaquete.value) * 899;
     }
-    total += parseInt(formPaquetes.diasPaquete.value) * 899;
 
-    if (formPaquetes.especial.checked)
-    {
+    if (formPaquetes.especial.checked) {
         total += 19999;
     }
 
-    if (formPaquetes.bebidas.checked)
-    {
-        total += 699;
-    }
-    
-    if (formPaquetes.buceo.checked)
-    {
-        total += 699;
-    }
-    
-    if (formPaquetes.expedicion.checked)
-    {
-        total += 699;
-    }
-    
-    if (formPaquetes.fotografias.checked)
-    {
-        total += 699;
-    }
-    
-    if (formPaquetes.pesca.checked)
-    {
+    if (formPaquetes.bebidas.checked) {
         total += 699;
     }
 
+    if (formPaquetes.buceo.checked) {
+        total += 699;
+    }
+
+    if (formPaquetes.expedicion.checked) {
+        total += 699;
+    }
+
+    if (formPaquetes.fotografias.checked) {
+        total += 699;
+    }
+
+    if (formPaquetes.pesca.checked) {
+        total += 699;
+    }
 
     formPaquetes.totalPaquetes.value = total.toLocaleString('es-MX');
-}   
+}
+
+formPaquetes.especial.addEventListener("change", function() {
+    if (this.checked) {
+        formPaquetes.diasPaquete.value = "";
+        formPaquetes.diasPaquete.disabled = true;
+        formPaquetes.destino.value = "";
+    } else {
+        formPaquetes.diasPaquete.disabled = false;
+    }
+});
+
+formPaquetes.destino.addEventListener("change", function() {
+    if (this.value) {
+        formPaquetes.especial.checked = false;
+        formPaquetes.diasPaquete.disabled = false;
+    }
+});
